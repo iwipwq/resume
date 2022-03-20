3d2fd7415fbca2458c39edb21a3985baa9e30ae9
munhong,KIM <93499222+iwipwq@users.noreply.github.com>
2022-03-21 01:30:33 +0900

    Add files via upload

--- source/scripts/nav.js
+++ source/scripts/nav.js
@@ -1,17 +1,15 @@
 (function (){
-    const targetElem = document.querySelector('.menu-list');
-    const pixelLimit = 2;
+    const menuListElement = document.querySelector('.menu-list');
+    const transitionStartingPoint= 2;
 
     const CheckScrollAndToggleClass = function(elem,px) {
         const TOGGLE_CLASSNAME = 'init';
-        const menuList = elem;
-        const menuListTransformStartPixel = px;
-        const currentScrolledValue = scrollY;
-        const isScrollOverTheLimit = currentScrolledValue > menuListTransformStartPixel;
-        isScrollOverTheLimit ? menuList.classList.remove(TOGGLE_CLASSNAME) : menuList.classList.add(TOGGLE_CLASSNAME);
+        const currentScrolledAmount = scrollY;
+        const isScrollOverTheStartingPoint = currentScrolledAmount > px;
+        isScrollOverTheStartingPoint ? elem.classList.remove(TOGGLE_CLASSNAME) : elem.classList.add(TOGGLE_CLASSNAME);
     }
 
-    this.addEventListener('scroll',e => {
-        CheckScrollAndToggleClass(targetElem,pixelLimit)
+    this.addEventListener('scroll',() => {
+        CheckScrollAndToggleClass(menuListElement,transitionStartingPoint)
     });
 })();
